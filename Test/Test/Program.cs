20994cab1deb03a9b47227772f9847ea9a9543f7
ayaeis <54796973+ayaeis@users.noreply.github.com>
2020-09-01 12:20:02 +0200

    done

---
+++ Test/Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Are you alive?");
+            string life = Console.ReadLine();
+            bool isAlive = true;
+            if (life == "yes")
+            {
+                isAlive = true;
+            }
+            else
+            {
+                isAlive = false;
+            }
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Alive:" + isAlive);
+
+        }
+    }
+}
