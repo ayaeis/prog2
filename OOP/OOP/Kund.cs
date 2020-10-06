e5dc27c9c310ca2e6e1bbe8f5da46eb89a80f950
ayaeis <54796973+ayaeis@users.noreply.github.com>
2020-10-06 12:20:22 +0200

    oop

---
+++ OOP/OOP/Kund.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Kund
+    {
+        public List<Produkt> _shoppingCart = new List<Produkt>();
+
+        public string _name;
+    }
+}
---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        private static bool shopping;
+
+        static void Main(string[] args)
+        {
+            var kund = new Kund();
+            Console.WriteLine("What is your name?");
+            kund._name = Console.ReadLine();
+
+            Console.WriteLine("Do you wanna shop?");
+            string answer = Console.ReadLine();
+            bool shopping;
+            if (answer == "yes")
+            {
+                shopping = true;
+            }
+            else
+            {
+                shopping = false;
+            }
+            while (shopping == true)
+            {
+                var produkt = new Produkt();
+                Console.WriteLine("Write your  wanted item");
+                produkt._produktTyp = Console.ReadLine();
+
+                kund._shoppingCart.Add(produkt);
+                Console.WriteLine("do you want to add another item?");
+                string answer2 = Console.ReadLine();
+                if ( answer2 == "yes")
+                {
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("This is ur cart");
+            Console.WriteLine(kund._shoppingCart);
+        }
+    }
+}
